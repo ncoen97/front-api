9e8463819b3c198e5b44a97b25f790132e77e7cc
nicolas <ncoen97@gmail.com>
2020-03-18 12:28:47 -0300

    avanzando con crud cursos (no funcional)

--- src/components/Cursos.js
+++ src/components/Cursos.js
@@ -1,21 +1,73 @@
 import React from 'react';
-import Curso from './Curso'
 import Actions from '../actions'
 import { connect } from 'react-redux'
-import { AppBar, Toolbar, Typography} from '@material-ui/core'
+import { AppBar, Toolbar, Typography, InputBase, Button} from '@material-ui/core'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Collapse from '@material-ui/core/Collapse'
 import { ExpandLess, ExpandMore } from '@material-ui/icons'
+import DeleteIcon from '@material-ui/icons/Delete';
 import PropTypes from 'prop-types'
-import { withStyles } from "@material-ui/core/styles";
+import { fade, withStyles } from "@material-ui/core/styles";
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import TextField from '@material-ui/core/TextField';
 
 const styles = theme => ({
     root: {
         width: "100%",
         maxWidth: 360,
-        background: theme.palette.background.paper
+        background: theme.palette.background.paper,
+    },
+    button: {
+        padding: "30px"
+    },
+    textField: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '25ch',
+          }
+    },
+    grow: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginRight: theme.spacing(2)
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block'
+        }
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(4),
+            width: 'auto',
+        },
+    },
+    inputRoot: {
+        color: 'inherit'
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 200
+        }
     }
 });
 
@@ -23,7 +75,7 @@ class Cursos extends React.Component{
 
     constructor(){
         super()
-        this.state = { cursos: [] }
+        this.state = { cursos: [], searchFilter: '' }
     }
 
     componentDidMount(){
@@ -33,31 +85,82 @@ class Cursos extends React.Component{
             this.setState({ cursos: data.message })
         })
     }
+
     handleClick = (e) => {
         this.setState({ [e]: !this.state[e] })
     }
 
+    handleValueChange =  (e) => {
+        console.log(e.target.value)
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+
     render(){
-        console.log(this.state.cursos)
+        console.log(this.state)
         const { classes } = this.props;
+        const cursosFiltrados = this.state.cursos.filter(curso => 
+            curso.tema.toLowerCase().includes(this.state.searchFilter.toLowerCase()))
         return (
-            <div>
+            <div className={classes.grow}>
                 <AppBar position="static">
-                    <Toolbar variant="dense">
-                        <Typography variant="h6" color="inherit">
+                    <Toolbar>
+                        <IconButton
+                            edge="start"
+                            className={classes.menuButton}
+                            color="inherit"
+                            aria-label="open drawer"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography className={classes.title} variant="h6" noWrap>
                             TP API REST!
                         </Typography>
+                        <div className={classes.search}>
+                            <InputBase
+                            placeholder="Tema..."
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                            name="searchFilter"
+                            value={this.state.searchFilter}
+                            onChange={this.handleValueChange}
+                            />
+                        </div>
+                        <div className={classes.grow} />
                     </Toolbar>
                 </AppBar>
-
+                <div className={classes.textField}>
+                    <Typography variant="h6">
+                        Agregar curso:
+                    </Typography>
+                    <TextField id="standard-basic" label="Tema" />
+                    <TextField id="standard-basic" label="Año" />
+                    <TextField id="standard-basic" label="Duracion" />
+                    <IconButton >
+                        <AddIcon />
+                    </IconButton>
+                </div>
+                <Typography variant="h6">
+                    Cursos actuales:
+                </Typography>
                 <List component="nav">
-                    {this.state.cursos.map(curso => (
+                    {
+                    cursosFiltrados.map(curso => (
                         <div key={curso._id}>
                             {curso.alumno != null ?  (
                                 <div key={curso._id}>
                                     <ListItem button key={curso._id} onClick={this.handleClick.bind(this, curso._id)} >
                                         <ListItemText primary={`Tema: ${curso.tema} - Año: ${curso.anio_de_dictado}
-                                         - Duracion: ${curso.duracion}`} />
+                                         - Duracion: ${curso.duracion}h`} />
+                                        <ListItemSecondaryAction className={classes.button}>
+                                            <IconButton aria-label="delete">
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </ListItemSecondaryAction>
                                         {this.state[curso._id] ? <ExpandLess /> : <ExpandMore />}
                                     </ListItem>
                                     <Collapse key={this.state.cursos._id} component="li" in={this.state[curso._id]} 
@@ -66,8 +169,14 @@ class Cursos extends React.Component{
                                             {curso.alumno.map((alumno) => {
                                                 return (
                                                     <ListItem button key={alumno._id} className={classes.nested}>
-                                                        <ListItemText key={alumno._id} primary={`DNI: ${alumno.DNI} - Nombre: 
-                                                        ${alumno.nombre} - Apellido: ${alumno.apellido}`} />
+                                                        <ListItemText key={alumno._id} primary={`- DNI: ${alumno.DNI} - Nombre: 
+                                                        ${alumno.nombre} - Apellido: ${alumno.apellido} - Nota: ${alumno.nota}`} />                                                        
+                                                        <ListItemSecondaryAction>
+                                                            <IconButton aria-label="delete" 
+                                                                onClick={() => Actions.dropCurso(curso)}>
+                                                                <DeleteIcon />
+                                                            </IconButton>
+                                                        </ListItemSecondaryAction>
                                                     </ListItem> 
                                                 )
                                             })}
